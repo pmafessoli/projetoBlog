5a1ee6680dd113974d5a5a4bb64b7467d678aa87
Pablo Mafessoli <pipo_nt@hotmail.com>
2022-10-03 10:34:36 -0300

    inovação: criação dos novos modelos, pedido, itens pedido e perfil.

--- blog/settings.py
+++ blog/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'categorias',
     'comentarios',
     'produto',
+    'pedido',
+    'perfil',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
